a4dc78866fd4348fb68112eb14b671078982b3f1
Seladji <cavalierabdellah@gmail.com>
2021-10-25 21:21:44 +0200

    ajout de fichier info.php

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php
-echo "salut";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ma page php</title>
+</head>
+<body>
+<h1>Ma page web</h1>
+        <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+</body>
+</html>
