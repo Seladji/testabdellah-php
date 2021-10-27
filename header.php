da0546792c29bfdd537c4c1ef047eab203d8f7e8
Seladji <cavalierabdellah@gmail.com>
2021-10-27 20:32:53 +0200

    implementation des pages

---
+++ header.php
@@ -0,0 +1,5 @@
+<article>
+                    <h3><?php echo $recipe['title']; ?></h3>
+                    <div><?php echo $recipe['recipe']; ?></div>
+                    <i><?php echo $recipe['author']; ?></i>
+</article>
--- index.php
+++ index.php
@@ -1,19 +1,46 @@
+<?php
+
+$recipes = [
+    [
+        'title' => 'Cassoulet',
+        'recipe' => 'Etape 1 : des flageolets !',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Couscous',
+        'recipe' => 'Etape 1 : de la semoule',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => false,
+    ],
+    [
+        'title' => 'Escalope milanaise',
+        'recipe' => 'Etape 1 : prenez une belle escalope',
+        'author' => 'mathieu.nebra@exemple.com',
+        'is_enabled' => true,
+    ],
+];
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ma page php</title>
+    <title>Affichage des recettes</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
 </head>
 <body>
-<h1>Ma page web</h1>
-<?php $chickenRecipesEnabled = false; ?>
-
-<?php if ($chickenRecipesEnabled): ?> <!-- Ne pas oublier le ":" -->
-
-<h1>Liste des recettes à base de poulet</h1>
-
-<?php endif; ?><!-- Ni le ";" après le endif -->
+    
+    <div class="container">
+        <h1>Affichage des recettes</h1>
+        <!-- Boucle sur les recettes -->
+        <?php foreach($recipes as $recipe) : ?>
+            <!-- si la clé existe et a pour valeur "vrai", on affiche -->
+            <?php if (array_key_exists('is_enabled', $recipe) && $recipe['is_enabled'] == true): ?>
+                <?php include('header.php'); ?>
+            <?php endif; ?>
+        <?php endforeach ?>
+    </div>   
 </body>
 </html>
